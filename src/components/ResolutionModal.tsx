76fbee88033613bfe41ae3e4dbb3f9ca084b8707
Ashok Nath <anath@awesomemotive.com>
2025-09-11 16:53:11 -0600

    Fixing lint error

--- src/components/ResolutionModal.tsx
+++ src/components/ResolutionModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { PendingItem, BankAccount, ResolutionData } from '../types';
 import { safedropResolutionApi } from '../services/api';
 
